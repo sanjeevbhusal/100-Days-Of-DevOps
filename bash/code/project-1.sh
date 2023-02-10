b1a3e6d027a53904a6954bebcc27611f7829c8ba
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2023-02-10 20:38:49 +0545

    code: update bash script to check if the repository gets installed correctly

--- bash/code/project-1.sh
+++ bash/code/project-1.sh
@@ -5,9 +5,21 @@
 echo -e "This script is used to download a git repository.\n"
 
 # Get the URL of the repository to clone 
-echo -n "Step 1: Enter the Url of the repository.: "
+echo -n "Step 1: Enter the Url of the repository: "
 read URL
 
 # Cloning the repository
 echo -e "\nStep 2: Cloning the repository\n"
 git clone $URL
+status=$?
+
+
+# Check if the repository is installed correctly
+echo ""
+
+if [ $status -eq 0 ]; then
+    echo -e "Sucess: Repository download succedded."
+else
+    echo -e "Error: Repositry download failed"
+    exit 1
+fi
