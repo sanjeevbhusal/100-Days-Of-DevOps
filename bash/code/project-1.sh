4a4b1811784701f3cfbbb93dd9b337653ca84595
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2023-02-10 19:31:50 +0545

    code: very basic bash script

---
+++ bash/code/project-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This bash script is used to download a github repository.
+
+echo -e "Downloading the repository\n"
+git clone https://github.com/sanjeevbhusal/100-Days-Of-DevOps.gits
