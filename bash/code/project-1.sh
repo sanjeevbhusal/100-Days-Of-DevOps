498f89d55b3858864e421364adb29e7b02baf168
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2023-02-12 21:17:53 +0545

    update ci

--- bash/code/project-1.sh
+++ bash/code/project-1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
 # This bash script is used to clone a github repository and start the application.
 
-# Set default value for the variable  
+
+# Define Variables  
 PROJECT_NAME="pythonApp"
 
 
-# Check if command line argument was supplied
+# If appropriate command line argument was supplied, update the variable PROJECT_NAME
 while getopts "n:" argument; do
     case $argument in 
         n) PROJECT_NAME="$OPTARG" ;;
@@ -15,49 +15,53 @@ while getopts "n:" argument; do
 done
 
 
-if [ -d $PROJECT_NAME ]; then
-    # If directory already exists, exit the script 
+# The cloned repository will be saved as $PROJECT_NAME. If a directory with this name already exist, exit the script. 
+if [ -d "$PROJECT_NAME" ]; then
     echo -e "Directory '$PROJECT_NAME' already exists. Pass a different name to the script using -n flag.\nExample: ./project-1.sh -n newname\nAborting the operation"
     exit 1
 fi
 
-# Get the URL of the repository to clone 
+
+# Clone the repository.
 echo -n "Step 1: Enter the Url of the repository: "
-read URL
+read -r URL
 
-# Cloning the repository
 echo -e "\nStep 2: Cloning the repository\n"
-git clone $URL $PROJECT_NAME
-
-# Check if the repository is installed correctly
-if [ $? -eq 0 ]; then
+if ! git clone "$URL" "$PROJECT_NAME"; then
     echo -e "Sucess: Repository download succedded\n"
 else
     echo -e "Error: Repositry download failed\n"
-    # exit 1
+    exit 1
 fi
 
+
 # Move inside projects directory
-cd $PROJECT_NAME
+cd "$PROJECT_NAME" || exit 
+
 
 # Create a virtual environment
 echo -e "Step 3: Creating the virtual environment\n"
 python3 -m venv venv
 
+
 # Activate Virtual environment
 echo -e "Step 4: Activating the virtual environment\n"
 source venv/bin/activate
+
 
 # Installing all the dependencies
 echo -e "Step 5: Installing all the dependencies\n"
 pip install -r requirements.txt
 
+
 # Seeding database
 echo -e -n "\nStep6: "
 python seed_database.py
 
+
 # Display success message
 echo -e "\nSucess: Project has been installed succesfully\n"
+
 
 # Running the project
 echo -e "Step6: Starting the Application Server..\n"
